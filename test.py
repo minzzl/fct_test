aa33d88d4645c5d1a2b6b1475e7172180101fa09
minzzl <minzzl.kim@lge.com>
2025-05-24 14:11:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,156 +1,47 @@
-void KeyEventTimer(lv_timer_t * timer)
+void* key_thread(void *arg)
 {
-	KeyCode* key1=lv_timer_get_user_data(timer);
-
-	if(key1->KeyCode == 0 && key1->KeyAction ==0)
-	{
-		lv_log("===============Left Key Relesed========================\n");
-
-		if(Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress == true && Ui_GetInstance()->mUiKeyLongPress.RightKeyPress == true)
-		{
-			if (Ui_GetInstance()->LongPressKeytimer != NULL){
-				lv_timer_delete(Ui_GetInstance()->LongPressKeytimer);
-				Ui_GetInstance()->LongPressKeytimer = NULL;
-				Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress = false;
-			}
-		}
-
-
-		if(Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress)
-		{
-			lv_obj_send_event(Ui_GetInstance()->mpEventKeyWidget,LV_EVENT_CLICKED,key1);
-		}
-		Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress = false;
-	}
-	else if(key1->KeyCode == 0 && key1->KeyAction ==1)
-	{
-		lv_log("===============Left Key Pressed========================\n");
-
-		pthread_mutex_lock(&serviceHandleMutex);
-		callPWM(serviceHandle);
-		pthread_mutex_unlock(&serviceHandleMutex);
-
-		Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress = true;
-
-		if(Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress == true && Ui_GetInstance()->mUiKeyLongPress.RightKeyPress == true)
-		{
-			static KeyCode keyLong={1,KEY_LR_LONG,KEY_PRESSED};
-			if (Ui_GetInstance()->LongPressKeytimer == NULL){
-				Ui_GetInstance()->LongPressKeytimer = lv_timer_create(LongPressKeyTimer, 10,&keyLong);
-				}
-		}
-	}
-	else if(key1->KeyCode == 2 && key1->KeyAction ==0)
-	{
-		lv_log("===============Right Key Relesed========================\n");
-
-		if(Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress == true && Ui_GetInstance()->mUiKeyLongPress.RightKeyPress == true)
-		{
-			if (Ui_GetInstance()->LongPressKeytimer != NULL){
-				lv_timer_delete(Ui_GetInstance()->LongPressKeytimer);
-				Ui_GetInstance()->LongPressKeytimer = NULL;
-				Ui_GetInstance()->mUiKeyLongPress.RightKeyPress = false;
-			}
-		}
-
-
-		if(Ui_GetInstance()->mUiKeyLongPress.RightKeyPress)
-		{
-			lv_obj_send_event(Ui_GetInstance()->mpEventKeyWidget,LV_EVENT_CLICKED,key1);
-		}
-		Ui_GetInstance()->mUiKeyLongPress.RightKeyPress = false;
-	}
-	else if(key1->KeyCode == 2 && key1->KeyAction ==1)
-	{
-		lv_log("===============Right Key Pressed========================\n");
-
-		pthread_mutex_lock(&serviceHandleMutex);
-		callPWM(serviceHandle);
-		pthread_mutex_unlock(&serviceHandleMutex);
-
-		Ui_GetInstance()->mUiKeyLongPress.RightKeyPress = true;
-
-		if(Ui_GetInstance()->mUiKeyLongPress.LeftKeyPress == true && Ui_GetInstance()->mUiKeyLongPress.RightKeyPress == true){
-			
-			static KeyCode keyLong={1,KEY_LR_LONG,KEY_PRESSED};
-			if (Ui_GetInstance()->LongPressKeytimer == NULL){
-				Ui_GetInstance()->LongPressKeytimer = lv_timer_create(LongPressKeyTimer, 10,&keyLong);
-
-			}
-		}
-	}
-	else if(key1->KeyCode == 1 && key1->KeyAction ==0)
-	{
-		lv_log("===============OK Key Pressed========================\n");
-		pthread_mutex_lock(&serviceHandleMutex);
-		callPWM(serviceHandle);
-		pthread_mutex_unlock(&serviceHandleMutex);
-
-		lv_obj_send_event(Ui_GetInstance()->mpEventKeyWidget,LV_EVENT_CLICKED,key1);
-	}
-	if (Ui_GetInstance()->EventTimer != NULL){
-		lv_timer_delete(Ui_GetInstance()->EventTimer);
-		Ui_GetInstance()->EventTimer = NULL;
-	}
-
-}
-
-void*  key_thread(void *arg)
-{
-	int ex_value1 = -1, ex_value2 = -1, ex_value3 = -1;
+    static int ex1 = -1, ex2 = -1, ex3 = -1;
 
     while (1) {
-        int value1 = read_gpio_value(LEFT_PIN);
-        int value2 = read_gpio_value(CENTER_PIN);
-        int value3 = read_gpio_value(RIGHT_PIN);
+        int v1 = read_gpio_value(LEFT_PIN);
+        int v2 = read_gpio_value(CENTER_PIN);
+        int v3 = read_gpio_value(RIGHT_PIN);
+        if (v1 == -1 || v2 == -1 || v3 == -1) continue;
 
-        if (value1 == -1 || value2 == -1 || value3 == -1)
-            continue;
+        /* active-low → active-high 변환 */
+        v1 = !v1;  v2 = !v2;  v3 = !v3;
 
-
-		 // 값 반전
-        value1 = (value1 == 0) ? 1 : 0;
-        value2 = (value2 == 0) ? 1 : 0;
-        value3 = (value3 == 0) ? 1 : 0;
-
-		//printf("%d %d %d\n",value1,value2,value3);
-        if (value1 != ex_value1 || value2 != ex_value2 || value3 != ex_value3) {
-
-			// if (value1 != ex_value1 & value3 != ex_value3){
-			// 	printf("longkey\n");
-
-			// 	key.KeyCode = 3;
-			// 	key.KeyAction = 0;
-			// }
-            if (value1 != ex_value1) {
-				printf("left %d\n",value1);
-                key.KeyCode = 0; // Left key
-                key.KeyAction = value1;
-            } else if (value2 != ex_value2) {
-				printf("ok %d\n",value2);
-                key.KeyCode = 1; // OK key
-                key.KeyAction = value2;
-            } else if (value3 != ex_value3) {
-				printf("right %d\n",value3);
-                key.KeyCode = 2; // Right key
-                key.KeyAction = value3;
-            } 
-            printf("Setting to key value\n");
-            ex_value1 = value1;
-            ex_value2 = value2;
-            ex_value3 = value3;
-            
-            while (Ui_GetInstance()->EventTimer) {
-                usleep(1);
-            };
-            printf("Go to KeyEventTimer\n");
-			Ui_GetInstance()->EventTimer=lv_timer_create(KeyEventTimer,10,&key);
+        /* ------------- Left ------------- */
+        if (v1 != ex1) {
+            while (Ui_GetInstance()->EventTimer) usleep(1);
+            key.KeyCode   = 0;
+            key.KeyAction = v1;
+            Ui_GetInstance()->EventTimer =
+                lv_timer_create(KeyEventTimer, 10, &key);
         }
 
-    // Add a sleep to reduce CPU usage
-        usleep(10000); // 10ms
+        /* ------------- OK --------------- */
+        if (v2 != ex2) {
+            while (Ui_GetInstance()->EventTimer) usleep(1);
+            key.KeyCode   = 1;
+            key.KeyAction = v2;
+            Ui_GetInstance()->EventTimer =
+                lv_timer_create(KeyEventTimer, 10, &key);
+        }
+
+        /* ------------- Right ------------ */
+        if (v3 != ex3) {
+            while (Ui_GetInstance()->EventTimer) usleep(1);
+            key.KeyCode   = 2;
+            key.KeyAction = v3;
+            Ui_GetInstance()->EventTimer =
+                lv_timer_create(KeyEventTimer, 10, &key);
+        }
+
+        /* 모든 이벤트를 보낸 뒤에야 이전 값 업데이트 */
+        ex1 = v1;  ex2 = v2;  ex3 = v3;
+
+        /* 폴링 주기 10 ms */
+        usleep(10000);
     }
-
-
-
 }
