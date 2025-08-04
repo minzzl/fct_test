73a9fba905369a6252c6a25f99e3226530776d5a
minzzl <minzzl.kim@lge.com>
2025-08-04 19:38:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-[x] File sync error: EOF during negotiation
+PS C:\Users\USER\wifi_logs\1> whoami
+lge\minzzl.kim
+PS C:\Users\USER\wifi_logs\1> ssh root@192.168.1.101
+root@192.168.1.101's password:
+[root@webOSNano-unofficial ~]# scp -r /lg_rw lge\minzzl@192.168.1.100:C:\Users\USER\wifi_logs
+ 
+/usr/bin/dbclient: Connection to lgeminzzl@192.168.1.100:22 exited: Connect failed: Connection refused
+lost connection
