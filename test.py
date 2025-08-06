9afb7e4774be4101bd341ebb4d77e25722052eb4
minzzl <minzzl.kim@lge.com>
2025-08-06 14:31:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,73 @@
-root@be4c8ed29962:/home/root/app.lgap_comm# cargo --version
-cargo 1.75.0 (1d8b05cdd 2023-11-20)
-root@be4c8ed29962:/home/root/app.lgap_comm# rustc --version
-rustc 1.75.0 (82e1608df 2023-12-21)
+root@be4c8ed29962:/home/root/app.lgap_comm# rustc -vV | grep host
+host: x86_64-unknown-linux-gnu
+root@be4c8ed29962:/home/root/app.lgap_comm# env
+webos_applicationsdir=/usr/palm/applications
+OPENSSL_ENGINES=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/lib/engines-3
+OE_CMAKE_TOOLCHAIN_FILE=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/share/cmake/OEToolchainConfig.cmake
+M4=m4
+OECORE_TUNE_CCARGS= -mbranch-protection=standard
+OECORE_SDK_VERSION=tc18-2023.08-unofficial
+PKG_CONFIG_PATH=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux/usr/lib/pkgconfig:/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux/usr/share/pkgconfig
+HOSTNAME=be4c8ed29962
+GDB=aarch64-webos-linux-gdb
+SDKTARGETSYSROOT=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+SSH_AUTH_SOCK=/tmp/ssh-aoDrtYv9KHUv/agent.13627
+OECORE_BASELIB=lib
+CPP=aarch64-webos-linux-gcc -E  -mbranch-protection=standard -fstack-protector-all  -O2 -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security -Werror=format-security -Werror=return-type -funwind-tables --sysroot=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+TARGET_PREFIX=aarch64-webos-linux-
+OE_CMAKE_FIND_LIBRARY_CUSTOM_LIB_SUFFIX=
+SSH_AGENT_PID=13628
+WEBOS_INSTALL_HEADERS=/usr/include
+WEBOS_INSTALL_QML=/usr/lib/qml
+PWD=/home/root/app.lgap_comm
+OECORE_TARGET_OS=linux
+CXX=aarch64-webos-linux-g++  -mbranch-protection=standard -fstack-protector-all  -O2 -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security -Werror=format-security -Werror=return-type -funwind-tables --sysroot=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+CXXFLAGS= -O2 -pipe -g -feliminate-unused-debug-types 
+OECORE_NATIVE_SYSROOT=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux
+LDFLAGS=-Wl,-O1 -Wl,--hash-style=gnu -Wl,--as-needed  -Wl,-z,relro,-z,now
+HOME=/root
+LS_COLORS=rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:
+OPENSSL_CONF=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/lib/ssl-3/openssl.cnf
+SSL_CERT_DIR=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/lib/ssl-3/certs
+KCFLAGS=--sysroot=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+OECORE_TARGET_SYSROOT=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+CPPFLAGS=
+OPENSSL_MODULES=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/lib/ossl-modules/
+webos_preferencesdir=/usr/preferences
+LD=aarch64-webos-linux-ld  --sysroot=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+TUNE_FEATURES=aarch64
+READELF=aarch64-webos-linux-readelf
+RPM_NO_CHROOT_FOR_SCRIPTS=1
+TERM=xterm
+AR=aarch64-webos-linux-ar
+ARCH=arm64
+AS=aarch64-webos-linux-as 
+SHLVL=1
+NM=aarch64-webos-linux-nm
+OECORE_TARGET_ARCH=aarch64
+WEBOS_INSTALL_LIBS=/usr/lib
+WEBOS_INSTALL_QTPLUGINSDIR=/usr/lib/plugins
+WEBOS_INSTALL_BINS=/usr/bin
+SSL_CERT_FILE=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/lib/ssl-3/certs/ca-certificates.crt
+OECORE_DISTRO_VERSION=15.0.0-s2
+PKG_CONFIG_SYSROOT_DIR=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+OECORE_ACLOCAL_OPTS=-I /usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/share/aclocal
+OBJCOPY=aarch64-webos-linux-objcopy
+STRIP=aarch64-webos-linux-strip
+OBJDUMP=aarch64-webos-linux-objdump
+CONFIG_SITE=/usr/local/nano-sdk-x86_64/site-config-aarch64-webos-linux
+PATH=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/bin:/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/sbin:/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/bin:/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/sbin:/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/bin/../x86_64-nanosdk-linux/bin:/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/bin/aarch64-webos-linux:/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/bin/aarch64-webos-linux-musl:/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+CC=aarch64-webos-linux-gcc  -mbranch-protection=standard -fstack-protector-all  -O2 -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security -Werror=format-security -Werror=return-type -funwind-tables --sysroot=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+CFLAGS= -O2 -pipe -g -feliminate-unused-debug-types 
+RPM_ETCCONFIGDIR=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/etc
+CROSS_COMPILE=aarch64-webos-linux-
+MAGIC=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/share/misc/magic.mgc
+CONFIGURE_FLAGS=--target=aarch64-webos-linux --host=aarch64-webos-linux --build=x86_64-linux --with-libtool-sysroot=/usr/local/nano-sdk-x86_64/sysroots/aarch64-webos-linux
+RANLIB=aarch64-webos-linux-ranlib
+RPM_CONFIGDIR=/usr/local/nano-sdk-x86_64/sysroots/x86_64-nanosdk-linux/usr/lib/rpm
+OLDPWD=/home/root
+_=/usr/bin/env
+
+
+
+나는 aarch64-webos-linux 이거 아닌가?
