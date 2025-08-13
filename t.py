f7baea62c1c2885c4b656b98c7cf28c5b986d144
minzzl <minzzl.kim@lge.com>
2025-08-13 17:58:49 +0900

    Update t.py

--- t.py
+++ t.py
@@ -1,9 +1,5 @@
 /home/root/app.acp_lcd/src/app/DataLogging.c: In function 'Create_DataLogging':
-/home/root/app.acp_lcd/src/app/DataLogging.c:70:28: error: too many arguments to function 'lv_spinner_create'
-In file included from /home/root/app.acp_lcd/./inc/lvgl/lvgl.h:75,
-                 from /home/root/app.acp_lcd/./inc/DataLogging.h:17,
-                 from /home/root/app.acp_lcd/src/app/DataLogging.c:15:
-/home/root/app.acp_lcd/./inc/lvgl/src/widgets/spinner/lv_spinner.h:43:12: note: declared here
+/home/root/app.acp_lcd/src/app/DataLogging.c:87:29: error: invalid storage class for function 'anim_cb_set_start'
 make[2]: *** [CMakeFiles/lcd_app.dir/build.make:622: CMakeFiles/lcd_app.dir/src/app/DataLogging.c.o] Error 1
 make[2]: *** Waiting for unfinished jobs....
 make[1]: *** [CMakeFiles/Makefile2:84: CMakeFiles/lcd_app.dir/all] Error 2
