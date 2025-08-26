4075e681219d095e333df54b2b446bda67991167
minzzl <minzzl.kim@lge.com>
2025-08-26 13:38:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,2 @@
-minzzl@minzzl-HP-Z6-G5-Workstation-Desktop-PC:~/Downloads$ journalctl -f 
-Aug 26 11:57:53 minzzl-HP-Z6-G5-Workstation-Desktop-PC citrix-ctxcwalogd[1464]: Failed to open "/log/citrix/ICAClient.log" for output: No such file or directory.
-Aug 26 11:57:53 minzzl-HP-Z6-G5-Workstation-Desktop-PC citrix-ctxcwalogd[1464]: Failed to open new output path, continuing to log to old path.
+minzzl@minzzl-HP-Z6-G5-Workstation-Desktop-PC:~/Downloads$ systemctl cat citrix-ctxcwalogd
+No files found for citrix-ctxcwalogd.service.
