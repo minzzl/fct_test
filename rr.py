802a28502133a0d09a39bd32ad272765f7de8a31
minzzl <minzzl.kim@lge.com>
2025-08-29 15:57:26 +0900

    Update rr.py

--- rr.py
+++ rr.py
@@ -1,68 +1,12 @@
-해당 부분 확인해주세요
+We talked on 10.175.195.66 every time we talked, and there was always vent and dxhrv here
  
-그리고 acp5 의 vent 및 dxhrv 장치의 제어패널과 현재 제어 패널이 정말 똑같은지 확인해주세요
+I cant push the code without testing on dummy data
  
-제가 봤을 때 다른 부분이 많습니다.
+I don't understand your attitude right now
  
-김민지/연구원/네트워크제어개발팀
-http://jira.lge.com/issue/projects/NETGUIAI/issues/NETGUIAI-679?filter=allopenissues
-I have added comment please check
+oh
  
-ok
+66 was closed since last 2 weeks. thank you. it is working now
  
-답글을 달았는데요, 이미 해당 아이콘 있습니다
- 
-안녕하세요
- 
-yes i ll ginish it today
- 
-넵 
- 
-현재 진행 중인게 어떤 것들인가요?
- 
-vent
- 
-네.
- 
-정확히 어떤거죠?
- 
-http://jira.lge.com/issue/browse/NETGUIAI-206?attachmentSortBy=dateTime&attachmentOrder=asc
-확인해주세요
- 
-okay
- 
-김민지/연구원/네트워크제어개발팀
-정확히 어떤거죠?
-이거에 답좀 해주시겠어요?
- 
-안녕하세요??
- 
-give me 5mins please
- 
-i am working on complete vent related tickets. there are so many in the jira on my name and many are duplicate
- 
-What content is duplicated?
- 
-After testing further, I found that almost every click leads to errors
- 
-ok. i will resolve them
-김민지/연구원/네트워크제어개발팀
-After testing further, I found that almost every click leads to errors
- 
- 
-wehre i can see vent devices?
- 
-hello?
- 
-I cant work on any of the vent device. moving to other issues
- 
-sorry?
- 
-What are you talking about with me?
- 
-I cant find any Vent Device? Can you tell me on which url should i look for it?
- 
-I've told you several times since this morning to fix the device, and when I asked you what you were doing, you said you were developing a vent device
- 
-I am. but I need a actual vent device for the testing
+Then how on earth did you test it before?
  
